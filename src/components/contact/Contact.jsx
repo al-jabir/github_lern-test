4fdbb1e66dc077db08a0cfeee4f6e1e7ba37c6e4
al-jabir <aljabir098@gmail.com>
2022-03-18 17:28:48 +0600

    added the contact style and text

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,10 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>Contact
+      <h1>I will be developer</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In, ducimus neque rem temporibus nulla quibusdam voluptates consequuntur tempore magni ut? Necessitatibus dicta itaque ducimus doloribus.</p>
+    </div>
   )
 }
 
